8154c6d022447c330d5cbf2389f0ca76defa6fed
Shan-Li Wang <59712863+kimberly332@users.noreply.github.com>
2024-10-22 23:18:45 -0400

    Q1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
 console.log("fired");
+
+let A = undefined; 
+let B = undefined;
+const initialDisplay = "❌"; // Red cross mark emoji to be displayed when value is unset. This will happen in intial render.
+
+function setA(value) {
+    A = value;
+    updateOutput(A, B);
+}
+
+function setB(value) {
+    B = value;
+    updateOutput(A, B);
+}
+
+function setInputs() {
+    const inputA = document.getElementById("inputA").value;
+    const inputB = document.getElementById("inputB").value;
+
+    if (inputA) {
+        A = inputA === "1";
+    }
+
+    if (inputB) {
+        B = inputB === "1";
+    }
+
+    updateOutput(A, B);
+}
+
+function updateOutput(valueA, valueB) {
+    const outputElementA = document.getElementById("outputA");
+    const outputElementB = document.getElementById("outputB");
+
+    if (valueA === undefined) {
+        outputElementA.innerHTML = `A: ${initialDisplay}`;
+    } else {
+        outputElementA.innerHTML = `A: ${valueA}`;
+    }
+
+    if (valueB === undefined) {
+        outputElementB.innerHTML = `B: ${initialDisplay}`;
+    } else {
+        outputElementB.innerHTML = `B: ${valueB}`;
+    }
+}
+
+// Initialize output
+updateOutput(A, B);
