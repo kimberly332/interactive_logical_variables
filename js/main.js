795e70d9df249e0d618c00e85ddf85e9a64d942e
Shan-Li Wang <59712863+kimberly332@users.noreply.github.com>
2024-10-24 03:13:57 -0400

    Adding comments

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 console.log("fired");
 
+// Initialize variables for logical operations
 let A = true; 
 let B = true;
 const resultElement = document.getElementById("result-value");
 const tableBodyElement = document.getElementById("table-body");
 
+// Sets the value of variable A and updates the display
 function setA(value) {
     A = value;
     displaySettingValues(A, B);
 }
 
+// Sets the value of variable B and updates the display
 function setB(value) {
     B = value;
     displaySettingValues(A, B);
 }
 
+// Converts input values of "1" to true and any other value to false
 function setInputs() {
     const inputA = document.getElementById("inputA").value;
     const inputB = document.getElementById("inputB").value;
@@ -26,6 +30,7 @@ function setInputs() {
     resultElement.innerHTML = "";
 }
 
+// Updates the display to show current values of A and B
 function displaySettingValues(valueA, valueB) {
     const displayElementA = document.getElementById("display-inputA");
     const displayElementB = document.getElementById("display-inputB");
@@ -33,6 +38,7 @@ function displaySettingValues(valueA, valueB) {
     displayElementB.innerHTML = `B is ${valueB}`;
 }
 
+// Generates a new row in the truth table based on current A and B values
 function generateRow() {
     // Calculate
     const result1 = A && B;
@@ -42,6 +48,7 @@ function generateRow() {
     const result5 = (A || B) && !(A && B);
     const result6 = (A && B) || !(A || B);
     
+    // Create HTML row string using table data generator
     const rowValue = "<tr>" + 
         generateTableData(A) + 
         generateTableData(B) + 
@@ -52,9 +59,12 @@ function generateRow() {
         generateTableData(result5) +
         generateTableData(result6) + 
         "</tr>";
+    
+    // Append new row to table body
     tableBodyElement.innerHTML += rowValue;
 }
 
+// Generates HTML table
 function generateTableData(data) {
     return "<td>" + data + "</td>";
 }
